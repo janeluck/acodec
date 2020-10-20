62787cb02bf904305e25a3635254e7acdd3916c8
janeluck <janeluck158@gmail.com>
2020-10-20 21:55:11 +0800

    chore: remove data

--- app/controller/both.ts
+++ app/controller/both.ts
@@ -1,9 +1,13 @@
 import { Controller } from 'egg';
+import { ws, opt } from '../../jane/env';
 
 export default class BothController extends Controller {
   public async index() {
     const { ctx } = this;
-    const data = { list: [ 'egg', 'exo', 'fake love', 'sweet', 'psycho' ] };
+    const data = {
+      ws,
+      opt,
+    };
     await ctx.render('both', data);
   }
 }
